47416c5f6e8bb37f8f0648cb8e9fa0a824328e5d
Nicolas Boulé <boule.nicolas@hotmail.fr>
2024-10-17 18:23:48 +0200

    CORS Allow all

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_HOSTS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
